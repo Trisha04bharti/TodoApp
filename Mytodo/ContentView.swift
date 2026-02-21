863ac8db37d6929cd02680f2fc420c9771d6d1d7
Trisha Bharti <trishabharti444@gmail.com>
2026-02-21 15:18:22 +0530

    Initial Commit

---
+++ Mytodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mytodo
+//
+//  Created by Vikram Kumar on 21/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mytodo/MytodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MytodoApp.swift
+//  Mytodo
+//
+//  Created by Vikram Kumar on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MytodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
